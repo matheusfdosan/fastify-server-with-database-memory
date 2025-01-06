5ec4e5090bb59412ac95a1424c01997d2677ce6b
matheusfdosan <matheusfaustinoe20@gmail.com>
2025-01-06 19:31:52 -0300

    :wrench: chore: host config for render.com

--- server.js
+++ server.js
@@ -65,4 +65,7 @@ server.delete("/videos/:id", (req, res) => {
   return res.status(204).send()
 })
 
-server.listen({ port: process.env.PORT ?? 1324 })
+server.listen({
+  host: "0.0.0.0",
+  port: process.env.PORT ?? 1324,
+})
